92e0e5541ff69750a0f209d3b4650d6d4e7d2015
Tuan Bui <antivenomff@gmail.com>
2024-05-24 17:07:11 +0700

    fix att

--- mbw_sfc_integrations/sfc_integrations/attendance.py
+++ mbw_sfc_integrations/sfc_integrations/attendance.py
@@ -125,7 +125,8 @@ def list_attendance(employee, start_date, end_date):
 
 def update_attendance_monthly(doc, method=None):
 	# Lấy ngày tháng để truy xuất dữ liệu
-	create_date = doc.attendance_date
+	cr_date = doc.attendance_date
+	create_date =  datetime.strptime(cr_date, '%Y-%m-%d')
 	month = int(create_date.month)
 	year = int(create_date.year)
 	start_date_str = f'{year:04d}-{month:02d}-01'
