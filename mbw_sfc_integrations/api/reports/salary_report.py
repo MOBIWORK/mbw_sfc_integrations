f5fa6da658abe17ca16842bdf08e42203d632701
chuyendev <chuyendev@gmail.com>
2024-06-07 16:56:03 +0700

    service report salary

--- mbw_sfc_integrations/api/reports/salary_report.py
+++ mbw_sfc_integrations/api/reports/salary_report.py
@@ -3,13 +3,13 @@ from datetime import (datetime,timedelta)
 from frappe import _
 from mbw_sfc_integrations.api.validators import (validate_filter)
 from mbw_sfc_integrations.api.common import (gen_response,exception_handle)
-@frappe.whitelist(method="GET")
+@frappe.whitelist(methods="GET")
 def salary_report(**body):
     try:
-        month = validate_filter(type_check="require",value=body.get("month"))
-        year = validate_filter(type_check="require",value=body.get("year"))
+        month = int(validate_filter(type_check="require",value=body.get("month")))
+        year = int(validate_filter(type_check="require",value=body.get("year")))
         department = body.get("department")
-        employee = body.get("department")
+        employee = body.get("employee")
         page_size =  int(body.get('page_size', 20))
         page_number = int(body.get('page_number') ) if body.get('page_number') and int(body.get('page_number')) > 0 else 1
         start_date = datetime(year,month,1,0,0,0)
@@ -25,17 +25,21 @@ def salary_report(**body):
         if employee:
             filters.update({"department":department})
         fieldsget = ["name","employee","employee_name","department","designation","gross_pay","net_pay","grade"]
-        list_salary = frappe.db.get_list(doctype="Salary Slip",filter=filters,fields= fieldsget,start=(page_number-1)*page_size,page_length=page_size, pluck='name')
+        list_salary = frappe.db.get_list(doctype="Salary Slip",filters=filters,fields= fieldsget,start=(page_number-1)*page_size,page_length=page_size, pluck='name')
+        total = len(frappe.db.get_list(doctype="Salary Slip",filters=filters,pluck='name'))
         for salary in list_salary:
             doc_salary = frappe.get_doc("Salary Slip",salary.get("name")).as_dict()
             earnings = doc_salary.earnings
             deductions = doc_salary.deductions
             if len(earnings) > 0 :
                 for earning in earnings:
-                    list_salary.update({earning.salary_component: earning.year_to_date})
+                    list_salary.update({earning.abbr: earning.year_to_date})
             if len(deductions) > 0 :
                 for deduction in deductions:
-                    list_salary.update({deduction.salary_component: deduction.year_to_date})
-        return gen_response(200,"",list_salary)
+                    list_salary.update({deduction.abbr: deduction.year_to_date})
+        return gen_response(200,"",{
+            "data": list_salary,
+            "total":total
+        })
     except Exception as e:
         exception_handle(e)
