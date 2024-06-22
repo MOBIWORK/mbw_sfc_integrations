ff142197889074b94bd66fac21b865fc798a91df
chuyendev <chuyendev@gmail.com>
2024-06-22 11:57:43 +0700

    fix service report

--- mbw_sfc_integrations/api/reports/salary_report.py
+++ mbw_sfc_integrations/api/reports/salary_report.py
@@ -3,6 +3,7 @@ from datetime import (datetime,timedelta)
 from frappe import _
 from mbw_sfc_integrations.api.validators import (validate_filter)
 from mbw_sfc_integrations.api.common import (gen_response,exception_handle)
+import pydash
 @frappe.whitelist(methods="GET")
 def salary_report(**body):
     try:
@@ -19,25 +20,46 @@ def salary_report(**body):
         filters = {
             "start_date": [">=",start_date],
             "end_date": ["<=",end_date]
-        }
+        }   
+
+        filter_att = {"month": month,"year": year}
 
         if employee:
             filters.update({"employee":employee})
+           
         if employee:
             filters.update({"department":department})
+            filter_att.update({"department":department})
         fieldsget = ["name","employee","employee_name","department","designation","gross_pay","net_pay","grade"]
         list_salary = frappe.db.get_list(doctype="Salary Slip",filters=filters,fields= fieldsget,start=(page_number-1)*page_size,page_length=page_size)
+        employee_list = pydash.map_(list_salary,lambda x: x.employee)
+        filter_att.update({"employee":["in",employee_list]})
+        fields_get = [ "broken_shift_hours_monthly","straight_shift_hours_monthly","number_work_explain_absence_monthly","number_of_holiday_monthly","work_hours_straight_holidays_monthly","work_hours_broken_holidays_monthly"]
+        fields_get_goldsf = [ "ngay_cong_chuan","cong_bu","phu_cap_xa_nha"]
+        list_attendance  = frappe.db.get_list("SFC Attendance Monthly Report",filters = filter_att,fields=fields_get)
+        list_attendance_golfsf  = frappe.db.get_list("GoldSF HR Data",filters = filter_att,fields=fields_get_goldsf)
         total = len(frappe.db.get_list(doctype="Salary Slip",filters=filters,pluck='name'))
         for salary in list_salary:
             doc_salary = frappe.get_doc("Salary Slip",salary.get("name")).as_dict()
             earnings = doc_salary.earnings
             deductions = doc_salary.deductions
+            attendance_employee = pydash.find(list_attendance,lambda x:x.employee == salary.employee)
+            attendance_employee_gold = pydash.find(list_attendance_golfsf,lambda x:x.employee == salary.employee)
+            print("employee",salary.employee)
+            if attendance_employee : 
+                for key,value in attendance_employee.items():
+                    salary.update({key: value})
+            if attendance_employee_gold : 
+                for key,value in attendance_employee_gold.items():
+                    salary.update({key: value})
             if len(earnings) > 0 :
                 for earning in earnings:
                     salary.update({earning.abbr: earning.year_to_date})
             if len(deductions) > 0 :
                 for deduction in deductions:
                     salary.update({deduction.abbr: deduction.year_to_date})
+            tong_ngay_cong= salary.NCCG or 0 + salary.NCCT or 0 + salary.number_work_explain_absence_monthly or 0 + salary.number_of_holiday_monthly or 0 + salary.cong_bu or 0 + salary.NCCTL or 0 + salary.work_hours_broken_holidays_monthly or 0 
+            salary.update({"tong_cong":tong_ngay_cong})
         return gen_response(200,"",{
             "data": list_salary,
             "total":total
